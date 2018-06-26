0951236ea96fdf9ab0c538ef456182fb5a4e9bb2
aheumaier <andreas.heumaier@microsoft.com>
2018-06-26 15:45:30 +0200

    Started nested templetes creation

--- kubernetes-demo/tmpl/deploy.sh
+++ kubernetes-demo/tmpl/deploy.sh
@@ -15,9 +15,9 @@ ensureSuccess "az group deployment validate --resource-group $RG --template-file
 
 
 # Ensure resouce group
-ensureSuccess "az group create --name $RG --location $LOCATION"
-ensureSuccess "az group create --name ${RG}Networking --location $LOCATION"
-ensureSuccess "az group create --name ${RG}Cluster --location $LOCATION"
+#ensureSuccess "az group create --name $RG --location $LOCATION"
+#ensureSuccess "az group create --name ${RG}Networking --location $LOCATION"
+#ensureSuccess "az group create --name ${RG}Cluster --location $LOCATION"
 
 
 # Go for network deployment
